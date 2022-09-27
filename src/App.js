bebe8b878c08f539225a197ae9b9b5acfab29949
Jonathan Alvarado <jonathansteveen.alvarado.contractor@bbva.com>
2022-09-26 21:08:36 -0500

    new feature and components for controlPresupuesto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState("");
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  }
 
   return (
     <div className="App">
       <Header
-        presupuesto = {presupuesto}
-        setPresupuesto = {setPresupuesto}
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img src={IconoNuevoGasto} alt="icono nuevo gasto" onClick={handleNuevoGasto}/>
+        </div>
+      )}
+      {modal && <Modal setModal={setModal}/>}
     </div>
   );
 }
--- src/components/NuevoPresupuesto.js
+++ src/components/NuevoPresupuesto.js
@@ -1,20 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
+  const [mensaje, setMensaje] = useState("");
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault(e);
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje("No es un presupuesto valido");
+
+      return;
+    }
+    setMensaje("");
+    setIsValidPresupuesto(true);
+
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-      <form className="formulario">
+      <form onSubmit={handlePresupuesto} className="formulario">
         <div className="campo">
-            <label>Definir Presupuesto</label>
-            <input
-                className="nuevo-presupuesto"
-                type="text"
-                placeholder="Añade tu Presupuesto"
-                value={presupuesto}
-                onChange={ (e) => setPresupuesto(e.target.value)}
-            />
+          <label>Definir Presupuesto</label>
+          <input
+            className="nuevo-presupuesto"
+            type="number"
+            placeholder="Añade tu Presupuesto"
+            value={presupuesto}
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
+          />
         </div>
         <input type="submit" value="añadir" />
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
       </form>
     </div>
   );
